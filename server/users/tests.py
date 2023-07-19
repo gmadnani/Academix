f965d7d13a49e9a0bab219c299a001201d738300
Varun Nadgir <theminivann@gmail.com>
2023-07-18 22:49:02 -0400

    tests for Users, Utils
    create first tests for Users and Utils

--- server/users/tests.py
+++ server/users/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from .forms import RegisterForm, LoginForm
 
-# Create your tests here.
+# python manage.py test
+# python manage.py test users
+
+# Register Form
+class RegisterFormTestCase(TestCase):
+    def test_valid_register_form(self):
+        # dummy form data, success case
+        form_data = {
+            'username': 'username',
+            'email': 'student@university.com',
+            'password': 'rightpassword',
+            'password1': 'rightpassword',
+        }
+        form = RegisterForm(data=form_data)
+        # if not form.is_valid(): 
+        #    print(form.errors)  # more error details
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_register_form(self):
+        # dummy form data, fail case
+        form_data = {
+            'username': 'username',
+            'email': 'student@university.com',
+            'password': 'rightpassword',
+            'password1': 'wrongpassword'
+        }
+        form = RegisterForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+# Login Form
+class LoginFormTestCase(TestCase):
+    def test_valid_login_form(self):
+        # dummy form data, success case
+        form_data = {
+            'username': 'username',
+            'password': 'mypassw0rd'
+        }
+        form = LoginForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_login_form(self):
+        # dummy form data, fail case
+        form_data = {
+            'username': '',
+            'password': 'password'
+        }
+        form = LoginForm(data=form_data)
+        self.assertFalse(form.is_valid())
---
+++ server/utils/tests.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from unittest.mock import patch
+from .email_send import send_register_email
+
+# python manage.py test
+# python manage.py test utils
+
+# Activation Code
+class EmailSendTestCase(TestCase):
+    # create mock variables for send_mail and random_str
+    @patch('utils.email_send.random_str')
+    @patch('utils.email_send.send_mail')
+    def test_send_register_email(self, mock_send_mail, mock_random_str):
+        email = 'test@example.com'
+        code = 'example_code'  
+        mock_random_str.return_value = code
+        
+        # test for send_type = 'register'
+        send_register_email(email, send_type='register')
+
+        mock_send_mail.assert_called_once_with(
+            'Verification Code For Register From Academix',
+            'Please use the following link to activate your code: http://127.0.0.1:8000/users/activate/{0}'.format(code),
+            'academix673@gmail.com',
+            [email]
+        )
+
+        # test for send_type = 'forget'
+        send_register_email(email, send_type='forget')
+
+        mock_send_mail.assert_called_with(
+            'Verification Code For Resetting your password',
+            'Please use the following link to reset password: http://127.0.0.1:8000/users/reset_password/{0}'.format(code),
+            'academix673@gmail.com',
+            [email]
+        )
