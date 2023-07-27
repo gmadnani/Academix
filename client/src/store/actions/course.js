e7bb42274317dc64c9847d738caab592cf96a6fc
Baiyan Deng <105751432+baiyand@users.noreply.github.com>
2023-07-27 18:31:02 -0400

    Create course.js

---
+++ client/src/store/actions/course.js
@@ -0,0 +1,40 @@
+import * as actionTypes from "./actiontypes";
+import axios from 'axios';
+
+export const fetchCoursesStart = () => {
+  return {
+    type: actionTypes.FETCH_COURSES_START
+  };
+};
+
+export const fetchCoursesSuccess = courses => {
+  return {
+    type: actionTypes.FETCH_COURSES_SUCCESS,
+    courses: courses
+  };
+};
+
+export const fetchCoursesFail = error => {
+  return {
+    type: actionTypes.FETCH_COURSES_FAIL,
+    error: error
+  };
+};
+
+export const fetchCourses = (token) => {
+  return dispatch => {
+    dispatch(fetchCoursesStart());
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`
+    };
+    axios.get('http://127.0.0.1:8000/courses/list/')
+      .then(res => {
+        const courses = res.data;
+        dispatch(fetchCoursesSuccess(courses));
+      })
+      .catch(err => {
+        dispatch(fetchCoursesFail(err));
+      });
+  };
+};
