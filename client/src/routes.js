d4e3c9b0132af9762858f7effe046360cf64d253
Baiyan Deng <105751432+baiyand@users.noreply.github.com>
2023-08-01 18:03:53 -0400

    Update routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -6,12 +6,14 @@ import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import HomepageLayout from "./containers/Home";
 import StudentHome from "./containers/StudentHome";
+import TeacherHome from "./containers/TeacherHome";
 
 const BaseRouter = () => (
   <Hoc>
     <Route path="/login" component={Login} />
     <Route path="/signup" component={Signup} />
     <Route path="/student" component={StudentHome}/>
+    <Route path="/teacher" component={TeacherHome}/>
     <Route exact path="/" component={HomepageLayout} />
   </Hoc>
 );
