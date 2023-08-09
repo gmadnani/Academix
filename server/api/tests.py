769eb5e75083d71be81283095cd356d56c77116a
Ajay Krishna Reddy <67040284+Ajay1030@users.noreply.github.com>
2023-08-09 16:31:03 -0400

    Assignment test.py

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,40 +1,60 @@
 from django.test import TestCase
-from datetime import datetime, timedelta
-from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib.auth.models import User
+from datetime import datetime,timedelta
 from django.contrib.auth import get_user_model
-from django.utils import timezone  # Import timezone module
-from .models import Assignment, CoursesList
+from .models import Assignment, GradedAssignment, Choice, Question, AssignmentSubmission, AssignmentGrading
+from django.utils import timezone
+from courses.models import CoursesList
 
-# Create your tests here
 User = get_user_model()
 
 class AssignmentTestCase(TestCase):
     def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword'
+        )
         self.course = CoursesList.objects.create(
-            courseID="CS101",
-            courseSemester="Spring",
-            courseYear=2023,
-            courseName="Introduction to Computer Science",
-            courseDescription="An introductory course to programming.",
-            owner=User.objects.create_user(username="teacher1", password="teacherpass")
+            courseName='Intro to CS', courseDescription='An introductory course to Computer Science', owner=User.objects.create_user(username="professor",password="122345")
         )
         self.due_date = timezone.now() + timedelta(days=7)
         self.assignment = Assignment.objects.create(
-            title="Test Assignment",
+            title='Test Assignment',
             courseId=self.course,
             due_date=self.due_date,
             full_grade=100,
-            assignment_files=SimpleUploadedFile("test_file.txt", b"file_content"),
+            assignment_files='test_file.txt',
         )
 
-def test_assignment_creation(self):
-    self.assertEqual(self.assignment.title, "Test Assignment")
-    self.assertEqual(self.assignment.courseId, self.course)
-    self.assertEqual(self.assignment.due_date, self.due_date)
-    self.assertEqual(self.assignment.full_grade, 100)
-    
-    expected_file_name_prefix = "uploads/test_file_"
-    self.assertTrue(self.assignment.assignment_files.name.startswith(expected_file_name_prefix))
+    def test_assignment_creation(self):
+        assignment = Assignment.objects.get(title='Test Assignment')
+        self.assertEqual(assignment.courseId, self.course)
+        self.assertEqual(assignment.due_date, self.due_date)
+        self.assertEqual(assignment.full_grade, 100)
+        self.assertEqual(str(assignment), 'Test Assignment')
 
-    def test_assignment_str(self):
-        self.assertEqual(str(self.assignment), "Test Assignment")
+class GradedAssignmentTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        self.course = CoursesList.objects.create(
+            courseName='Intro to CS', courseDescription='An introductory course to Computer Science',owner=User.objects.create_user(username="professor",password="122345")
+        )
+        self.due_date = timezone.now() + timedelta(days=7)
+        self.assignment = Assignment.objects.create(
+            title='Test Assignment',
+            courseId=self.course,
+            due_date=self.due_date,
+            full_grade=100,
+            assignment_files='test_file.txt',
+        )
+        self.graded_assignment = GradedAssignment.objects.create(
+            student=self.user, assignment=self.assignment, grade=95
+        )
+
+    def test_graded_assignment_creation(self):
+        graded_assignment = GradedAssignment.objects.get(student=self.user)
+        self.assertEqual(graded_assignment.assignment, self.assignment)
+        self.assertEqual(graded_assignment.grade, 95)
+        self.assertEqual(str(graded_assignment), 'testuser')
+        
