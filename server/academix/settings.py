21193dfe36d2c3a103958790e86ab2791c66134c
astlian9 <135862701+astlian9@users.noreply.github.com>
2023-08-12 19:29:43 -0400

    changing hierarchy

--- server/academix/settings.py
+++ server/academix/settings.py
@@ -135,12 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'users/static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # setting the url where user upload their files
 MEDIA_URL = 'media/'
