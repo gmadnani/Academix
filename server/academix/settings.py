8b1fe8db67330f0836b1b604beb9181f2c70a710
astlian9 <135862701+astlian9@users.noreply.github.com>
2023-08-12 22:08:08 -0400

    configuration

--- server/academix/settings.py
+++ server/academix/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&9c!(4ulg(%b(%qh-vy-wg+os5+739sb4rhzmcn7tf@_i%%mvc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['https://academix-back-f296f40df966.herokuapp.com/',  'localhost', '127.0.0.1']
@@ -135,12 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles'),
-# ]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/var/www/academix-back-f296f40df966.herokuapp.com/static/"
 
 # setting the url where user upload their files
 MEDIA_URL = 'media/'
