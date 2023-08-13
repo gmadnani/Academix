24c48fc3b8946869ed2f4588d9c29cf5b283a1f6
Girish <45308550+gmadnani@users.noreply.github.com>
2023-08-13 02:30:58 -0400

    teacher can manually mark attendance

--- client/src/containers/AttendenceDetails.js
+++ client/src/containers/AttendenceDetails.js
@@ -1,38 +1,106 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { Segment, List, Header } from 'semantic-ui-react';
-import { fetchAttendanceStudentList } from '../store/actions/attendance';
+import { Segment, List, Header, Message } from "semantic-ui-react";
+import { fetchAttendanceStudentList } from "../store/actions/attendance";
+import axios from "axios"; // Import axios
 
-const AttendanceDetails = ({ token, attendanceID, studentList, loadingStudentList, errorStudentList, fetchAttendanceStudentList }) => {
+const AttendanceDetails = ({
+  token,
+  attendanceID,
+  studentList,
+  loadingStudentList,
+  errorStudentList,
+  fetchAttendanceStudentList,
+}) => {
+  const [updatedAttendance, setUpdatedAttendance] = useState({});
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
   useEffect(() => {
     fetchAttendanceStudentList(token, attendanceID);
   }, [fetchAttendanceStudentList, token, attendanceID]);
+
+  const handleUpdateAttendance = (studentID, newAttendanceStatus) => {
+    setUpdatedAttendance({
+      ...updatedAttendance,
+      [studentID]: newAttendanceStatus,
+    });
+  };
+
+  const handleSubmitAttendance = async () => {
+    const updatedStudentIDs = Object.keys(updatedAttendance);
+
+    if (updatedStudentIDs.length > 0) {
+      const updatePromises = updatedStudentIDs.map(async (studentID) => {
+        const newAttendanceStatus = updatedAttendance[studentID];
+        try {
+          await axios.put(
+            `http://127.0.0.1:8000/attendance/detail/${attendanceID}/`,
+            {
+              studentID: studentID,
+              if_attended: newAttendanceStatus,
+            },
+            {
+              headers: {
+                Authorization: `Token ${token}`,
+              },
+            }
+          );
+          setShowSuccessMessage(true);
+          return true;
+        } catch (error) {
+          console.error(`Error updating attendance for ${studentID}:`, error);
+          return false;
+        }
+      });
+
+      Promise.all(updatePromises).then(() => {
+        fetchAttendanceStudentList(token, attendanceID);
+      });
+    }
+  };
 
   return (
     <div>
       <Header as="h3" textAlign="center">
         Student List for Attendance
       </Header>
+      {showSuccessMessage && (
+        <Message
+          success
+          header="Attendance Updated"
+          content="Attendance has been updated successfully!"
+        />
+      )}
       {loadingStudentList ? (
         <p>Loading student list...</p>
       ) : errorStudentList ? (
         <p>Error loading student list: {errorStudentList.message}</p>
       ) : (
-        // Add an extra check for studentList before using map
         studentList && studentList.length > 0 ? (
           <Segment>
             <List divided relaxed>
               {studentList.map((student) => (
-                <List.Item key={student.student_email}>
+                <List.Item key={student.student_username}>
                   <List.Content>
                     <List.Header>{student.student_username}</List.Header>
                     <List.Description>
-                      {student.student_email} - {student.if_attended ? 'Attended' : 'Absent'}
+                      {student.if_attended ? "Attended" : "Absent"}
+                      <input
+                        type="checkbox"
+                        checked={updatedAttendance[student.student_username] || student.if_attended}
+                        onChange={() =>
+                          handleUpdateAttendance(
+                            student.student_username,
+                            !updatedAttendance[student.student_username]
+                          )
+                        }
+                      />
                     </List.Description>
                   </List.Content>
                 </List.Item>
               ))}
             </List>
+            <button onClick={handleSubmitAttendance}>Update Attendance</button>
           </Segment>
         ) : (
           <p>No students found for this attendance.</p>
@@ -42,15 +110,13 @@ const AttendanceDetails = ({ token, attendanceID, studentList, loadingStudentLis
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    token: state.auth.token,
-    attendanceID: localStorage.getItem('attendanceID'),
-    studentList: state.attendance.studentList,
-    loading: state.attendance.loading,
-    error: state.attendance.error,
-  };
-};
+const mapStateToProps = (state) => ({
+  token: state.auth.token,
+  attendanceID: localStorage.getItem("attendanceID"),
+  studentList: state.attendance.studentList,
+  loading: state.attendance.loading,
+  error: state.attendance.error,
+});
 
 const mapDispatchToProps = {
   fetchAttendanceStudentList,
